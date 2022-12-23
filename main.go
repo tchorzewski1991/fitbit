aca146ed719297bb64df6523bb601d76fc46fbba
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2022-12-23 16:56:08 +0100

    Setup simple scaffold for run func

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Fitledger!")
+	logger := log.New(os.Stderr, "[fit-ledger] ", log.Lmicroseconds)
+
+	if err := run(logger); err != nil {
+		logger.Fatalln(err)
+	}
+}
+
+func run(logger *log.Logger) error {
+	logger.Println("service start")
+	defer logger.Println("service end")
+	return nil
 }
