f1a5481d376bfb7c75eb93d11011d9626002c29c
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-07 21:05:41 +0100

    Add initial version of a blockchain transaction

---
+++ core/blockchain/database/tx.go
@@ -0,0 +1,100 @@
+package database
+
+import (
+	"crypto/ecdsa"
+	"errors"
+	"fmt"
+	"github.com/tchorzewski1991/fitbit/core/blockchain/signature"
+	"math/big"
+	"time"
+)
+
+// Tx represents transactional change between two accounts.
+type Tx struct {
+	ChainID uint64    `json:"chain_id"`
+	Nonce   uint64    `json:"nonce"`
+	From    AccountID `json:"from"`
+	To      AccountID `json:"to"`
+	Value   uint64    `json:"value"`
+	Tip     uint64    `json:"tip"`
+	Data    []byte    `json:"data"`
+}
+
+// Sign uses ECDSA private key to sign the Tx.
+func (tx Tx) Sign(pk *ecdsa.PrivateKey) (SignedTx, error) {
+
+	// Sign tx with given private key
+	r, s, v, err := signature.Sign(tx, pk)
+	if err != nil {
+		return SignedTx{}, fmt.Errorf("failed to sign tx: %w", err)
+	}
+
+	return SignedTx{
+		Tx: tx,
+		R:  r,
+		S:  s,
+		V:  v,
+	}, nil
+}
+
+// SignedTx represents signed version of the transaction.
+// SignedTx is how clients like 3rd party wallets can include
+// transaction into the fitbit blockchain.
+type SignedTx struct {
+	Tx
+	R *big.Int `json:"r"`
+	S *big.Int `json:"s"`
+	V *big.Int `json:"v"`
+}
+
+// Verify checks whether the transaction has a proper signature.
+func (tx SignedTx) Verify(chainID uint64) error {
+
+	if tx.ChainID != chainID {
+		return fmt.Errorf("chain ID: %d is not valid", chainID)
+	}
+
+	if err := tx.From.Verify(); err != nil {
+		return fmt.Errorf("tx from: %s is not valid", tx.From)
+	}
+
+	if err := tx.To.Verify(); err != nil {
+		return fmt.Errorf("tx to: %s is not valid", tx.To)
+	}
+
+	if tx.From == tx.To {
+		return fmt.Errorf("cannot send from: %s to: %s", tx.From, tx.To)
+	}
+
+	pub, err := signature.RecoverPubkey(tx.Tx, tx.R, tx.S, tx.V)
+	if err != nil {
+		return errors.New("cannot recover address from signature")
+	}
+
+	txFrom := tx.From
+	signatureAddress := PubToAccountID(pub)
+
+	if txFrom != signatureAddress {
+		return fmt.Errorf("tx from: %s does not match signature address: %s", txFrom, signatureAddress)
+	}
+
+	return nil
+}
+
+// BlockTx represents transaction that will be saved to the blockchain.
+type BlockTx struct {
+	SignedTx
+	Timestamp uint64 `json:"timestamp"`
+	GasPrice  uint64 `json:"gas_price"`
+	GasUnits  uint64 `json:"gas_units"`
+}
+
+// NewBlockTx constructs a new block transaction.
+func NewBlockTx(tx SignedTx, gasPrice, gasUnits uint64) BlockTx {
+	return BlockTx{
+		SignedTx:  tx,
+		Timestamp: uint64(time.Now().UnixNano()),
+		GasPrice:  gasPrice,
+		GasUnits:  gasUnits,
+	}
+}
---
+++ core/blockchain/database/tx_test.go
@@ -0,0 +1,147 @@
+package database_test
+
+import (
+	"crypto/ecdsa"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/assert"
+	"github.com/tchorzewski1991/fitbit/core/blockchain/database"
+	"testing"
+)
+
+const defaultChainID = uint64(1)
+
+func TestTx_SignAndVerify(t *testing.T) {
+	priv := loadPrivateKey(t)
+	params := defaultTxParams(t)
+
+	// Build and sign valid tx with nil private key
+	tx := buildTx(&params)
+	signedTx, err := tx.Sign(nil)
+	assert.EqualError(t, err, "failed to sign tx: private key is mandatory")
+
+	// Build and sign valid tx with proper private key
+	signedTx, err = tx.Sign(priv)
+	assert.Nil(t, err)
+
+	// Verify signed tx with default chain ID
+	err = signedTx.Verify(defaultChainID)
+	assert.Nil(t, err)
+}
+
+func TestTx_SignAndVerifyChainID(t *testing.T) {
+	priv := loadPrivateKey(t)
+	params := defaultTxParams(t)
+
+	// Build and sign tx with default chain ID
+	tx := buildTx(&params)
+	signedTx, err := tx.Sign(priv)
+	assert.Nil(t, err)
+
+	// Verify signed tx with other chain ID
+	err = signedTx.Verify(2)
+	assert.EqualError(t, err, "chain ID: 2 is not valid")
+}
+
+func TestTx_SignAndVerifyFrom(t *testing.T) {
+	priv := loadPrivateKey(t)
+	params := defaultTxParams(t)
+
+	// Build and sign tx with empty from address
+	params.from = ""
+	tx := buildTx(&params)
+	signedTx, err := tx.Sign(priv)
+	assert.Nil(t, err)
+
+	// Verify signed tx with default chain ID
+	err = signedTx.Verify(defaultChainID)
+	assert.EqualError(t, err, "tx from:  is not valid")
+}
+
+func TestTx_SignAndVerifyTo(t *testing.T) {
+	priv := loadPrivateKey(t)
+	params := defaultTxParams(t)
+
+	// Build and sign tx with empty to address
+	params.to = ""
+	tx := buildTx(&params)
+	signedTx, err := tx.Sign(priv)
+	assert.Nil(t, err)
+
+	// Verify signed tx with default chain ID
+	err = signedTx.Verify(defaultChainID)
+	assert.EqualError(t, err, "tx to:  is not valid")
+}
+
+func TestTx_SignAndVerifyFromToEqual(t *testing.T) {
+	priv := loadPrivateKey(t)
+	params := defaultTxParams(t)
+
+	// Build and sign tx with from address the same as to address
+	params.from = params.to
+	tx := buildTx(&params)
+	signedTx, err := tx.Sign(priv)
+	assert.Nil(t, err)
+
+	// Verify signed tx with default chain ID
+	err = signedTx.Verify(defaultChainID)
+	assert.EqualError(t, err, "cannot send from: 0x0ce5ba68586c85880B0900D0dEe0eEcBB33040e0 to: 0x0ce5ba68586c85880B0900D0dEe0eEcBB33040e0")
+}
+
+func TestTx_SignAndVerifySignatureAddress(t *testing.T) {
+	priv := loadPrivateKey(t)
+	params := defaultTxParams(t)
+
+	// Build and sign tx
+	params.from = "0x0ee5ba68586c85880B0900D0dEe0eEcBB37010e1"
+	tx := buildTx(&params)
+	signedTx, err := tx.Sign(priv)
+	assert.Nil(t, err)
+
+	// Verify signed tx with default chain ID
+	err = signedTx.Verify(defaultChainID)
+	assert.EqualError(t, err, "tx from: 0x0ee5ba68586c85880B0900D0dEe0eEcBB37010e1 does not match signature address: 0x0ee5ba68586c85880B0900D0dEe0eEcBB37010e0")
+}
+
+// Helper functions
+
+type txParams struct {
+	chainID uint64
+	from    database.AccountID
+	to      database.AccountID
+}
+
+func defaultTxParams(t *testing.T) txParams {
+	return txParams{
+		chainID: defaultChainID,
+		from:    loadAccountID(t, "0x0ee5ba68586c85880B0900D0dEe0eEcBB37010e0"),
+		to:      loadAccountID(t, "0x0ce5ba68586c85880B0900D0dEe0eEcBB33040e0"),
+	}
+}
+
+func buildTx(params *txParams) database.Tx {
+	return database.Tx{
+		ChainID: params.chainID,
+		Nonce:   1,
+		From:    params.from,
+		To:      params.to,
+		Value:   100,
+		Tip:     10,
+		Data:    nil,
+	}
+}
+
+func loadPrivateKey(t *testing.T) *ecdsa.PrivateKey {
+	priv, err := crypto.LoadECDSA("testdata/test.ecdsa")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return priv
+}
+
+func loadAccountID(t *testing.T, hexID string) database.AccountID {
+	id, err := database.ToAccountID(hexID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return id
+}
