256bcd563f43dfd584e3c0930b733e99adf4e7c2
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-10 19:32:56 +0100

    Add package responsible for signature manipulations

---
+++ core/blockchain/signature/signature.go
@@ -0,0 +1,165 @@
+package signature
+
+import (
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/goccy/go-json"
+)
+
+// ZeroHash represents a hash code of zeros.
+const ZeroHash string = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+const fitbitID = 23
+
+// Sign signs any value using given private key.
+// Function returns r, s, v representation of the signature where:
+//   - r represents first coordinate of the ecdsa signature
+//   - s represents second coordinate of the ecdsa signature
+//   - v represents message signing ID, either 0 or 1 (23 or 24 with fitbitID)
+func Sign(value any, pk *ecdsa.PrivateKey) (r, s, v *big.Int, err error) {
+
+	if pk == nil {
+		return nil, nil, nil, fmt.Errorf("signature sign err: %w", errors.New("private key is mandatory"))
+	}
+
+	// Marshal value to the raw bytes (we need this for Keccak256 hash function)
+	data, err := json.Marshal(value)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("signature sign err: %w", err)
+	}
+
+	// We want to stamp our data to make sure it is our system that has produced the hash
+	stamp := []byte(fmt.Sprintf("\x19Fitbit Signed Message:\n%d", len(data)))
+
+	// Hash data with Keccak256 hash function to the fixed 32 bytes array
+	hash := crypto.Keccak256(stamp, data)
+
+	// Sign hashed data with the private key
+	sig, err := crypto.Sign(hash, pk)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("signature sign err: %w", err)
+	}
+
+	// Derive public key associated with the private key
+	pub := pk.Public()
+
+	// Ensure derived public key is of *ecdsa.PublicKey type
+	_, ok := pub.(*ecdsa.PublicKey)
+	if !ok {
+		return nil, nil, nil, fmt.Errorf("signature sign err: %w", errors.New("public key needs to be ecdsa"))
+	}
+
+	// Convert produced signature to r,s,v format
+	r, s, v, err = FromBytes(sig)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("signature sign err: %w", err)
+	}
+
+	return
+}
+
+// Verify takes the r, s, v signature representation and verifies its correctness.
+//   - r represents first coordinate of the ecdsa signature
+//   - s represents second coordinate of the ecdsa signature
+//   - v represents message signing ID, either 0 or 1 (23 or 24 with fitbitID)
+func Verify(r, s, v *big.Int) error {
+
+	// Message signing ID should be either 0 or 1.
+	uint64V := v.Uint64() - fitbitID
+	if uint64V != 0 && uint64V != 1 {
+		return errors.New("message signing id is invalid")
+	}
+
+	// Validate correctness of the given signature coordinates
+	if !crypto.ValidateSignatureValues(byte(uint64V), r, s, false) {
+		return errors.New("signature is invalid")
+	}
+
+	return nil
+}
+
+// RecoverPubkey takes any value and r, s, v signature representation and tries to recover
+// public key associated with that signature.
+func RecoverPubkey(value any, r, s, v *big.Int) (*ecdsa.PublicKey, error) {
+
+	// Marshal value to the raw bytes (we need this for Keccak256 hash function).
+	data, err := json.Marshal(value)
+	if err != nil {
+		return nil, fmt.Errorf("recover pubkey err: %w", err)
+	}
+
+	// We want to stamp our data to make sure it is our system that has produced the hash.
+	stamp := []byte(fmt.Sprintf("\x19Fitbit Signed Message:\n%d", len(data)))
+
+	// Hash data with Keccak256 hash function to the fixed 32 bytes array.
+	hash := crypto.Keccak256(stamp, data)
+
+	// Convert r, s, v signature format to the []byte signature representation.
+	sig, err := ToBytes(r, s, v)
+	if err != nil {
+		return nil, fmt.Errorf("recover pubkey err: %w", err)
+	}
+
+	// Extract public key associated with the hash and the signature.
+	pub, err := crypto.SigToPub(hash, sig)
+	if err != nil {
+		return nil, fmt.Errorf("recover pubkey err: %w", err)
+	}
+
+	return pub, err
+}
+
+func RecoverAddress(pub ecdsa.PublicKey) common.Address {
+	return crypto.PubkeyToAddress(pub)
+}
+
+// Hash returns unique representation of the value.
+func Hash(value any) string {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return ZeroHash
+	}
+
+	hash := sha256.Sum256(data)
+
+	return hexutil.Encode(hash[:])
+}
+
+func ToBytes(r, s, v *big.Int) ([]byte, error) {
+	if r == nil || s == nil || v == nil {
+		return nil, errors.New("cannot convert signature to bytes")
+	}
+
+	sig := make([]byte, crypto.SignatureLength)
+
+	rbs := make([]byte, 32)
+	r.FillBytes(rbs)
+	copy(sig[:32], rbs)
+
+	sbs := make([]byte, 32)
+	s.FillBytes(sbs)
+	copy(sig[32:], sbs)
+
+	sig[64] = byte(v.Uint64() - fitbitID)
+
+	return sig, nil
+}
+
+func FromBytes(sig []byte) (r, s, v *big.Int, err error) {
+	if len(sig) < 64 {
+		return nil, nil, nil, errors.New("cannot convert signature to r, s, v")
+	}
+
+	r = big.NewInt(0).SetBytes(sig[:32])
+	s = big.NewInt(0).SetBytes(sig[32:64])
+	v = big.NewInt(0).SetBytes([]byte{sig[64] + fitbitID})
+
+	return
+}
---
+++ core/blockchain/signature/signature_test.go
@@ -0,0 +1,72 @@
+package signature_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tchorzewski1991/fitbit/core/blockchain/signature"
+	"github.com/tchorzewski1991/fitbit/core/blockchain/testdata"
+)
+
+const (
+	from = "0x0ee5ba68586c85880B0900D0dEe0eEcBB37010e0"
+)
+
+func TestHash_Hashable(t *testing.T) {
+	hashable := struct {
+		X, Y int
+	}{
+		X: 1,
+		Y: 2,
+	}
+	hash := signature.Hash(hashable)
+	assert.Equal(t, "0x", hash[:2])
+}
+
+func TestHash_NonHashable(t *testing.T) {
+	nonHashable := struct {
+		X, Y chan int
+	}{}
+	hash := signature.Hash(nonHashable)
+	assert.Equal(t, signature.ZeroHash, hash)
+}
+
+func TestSigning(t *testing.T) {
+	priv := testdata.LoadPrivateKey(t)
+
+	data := struct {
+		Name string `json:"name"`
+	}{
+		Name: "John",
+	}
+
+	// Sign data with private key
+	r, s, v, err := signature.Sign(data, priv)
+	assert.Nil(t, err)
+
+	// Verify r, s, v signature
+	err = signature.Verify(r, s, v)
+	assert.Nil(t, err)
+
+	// Recover public key from data and r, s, v signature
+	pub, err := signature.RecoverPubkey(data, r, s, v)
+	assert.Nil(t, err)
+
+	// Recover address from public key
+	addr := signature.RecoverAddress(*pub)
+
+	// Match expected address with one extracted from public key
+	assert.Equal(t, from, addr.String())
+
+	// Convert r, s, v signature values to bytes format
+	bs, err := signature.ToBytes(r, s, v)
+	assert.Nil(t, err)
+
+	// Convert bytes signature to r, s, v format
+	recR, recS, recV, err := signature.FromBytes(bs)
+	assert.Nil(t, err)
+
+	assert.Equal(t, r, recR)
+	assert.Equal(t, s, recS)
+	assert.Equal(t, v, recV)
+}
