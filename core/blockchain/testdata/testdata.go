12df322e3b8005b9526bdb54022597e4a13ebb00
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-14 15:50:54 +0100

    Update how ecdsa priv key is initialized for testdata

--- core/blockchain/testdata/testdata.go
+++ core/blockchain/testdata/testdata.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadPrivateKey(t *testing.T) *ecdsa.PrivateKey {
-	priv, err := crypto.LoadECDSA("testdata/test.ecdsa")
+	priv, err := crypto.HexToECDSA("1cc3ea5a590a4181339569fcd7771cb2d95f203143f116b4cb66556c058e59c4")
 	if err != nil {
 		t.Fatal(err)
 	}
