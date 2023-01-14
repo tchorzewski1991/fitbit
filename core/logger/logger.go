7f54bc9dd55009b6d8c6d369fb731b4a4c7a0390
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-15 00:54:44 +0100

    Disable caller in zap logger config

--- core/logger/logger.go
+++ core/logger/logger.go
@@ -8,6 +8,7 @@ import (
 func New(service string, build string) (*zap.SugaredLogger, error) {
 	config := zap.NewProductionConfig()
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableCaller = true
 	config.InitialFields = map[string]any{
 		"service": service,
 		"version": build,
