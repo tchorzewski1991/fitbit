bd023af664c691458c1368d2b271dec1ade5743e
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-16 20:30:22 +0100

    Ensure gin is set to release mode - hardcoded for now

--- app/services/node/handlers/handlers.go
+++ app/services/node/handlers/handlers.go
@@ -16,6 +16,8 @@ type Config struct {
 // PublicMux constructs a new http.Handler and registers all public routes.
 // Every new version of public routes should be registered here.
 func PublicMux(cfg Config) http.Handler {
+	gin.SetMode(gin.ReleaseMode)
+
 	mux := gin.New()
 
 	v1.PublicHandlers(mux, v1.Config{
@@ -28,6 +30,8 @@ func PublicMux(cfg Config) http.Handler {
 // PrivateMux constructs a new http.Handler and registers all private routes.
 // Every new version of private routes should be registered here.
 func PrivateMux(cfg Config) http.Handler {
+	gin.SetMode(gin.ReleaseMode)
+
 	mux := gin.New()
 
 	v1.PrivateHandlers(mux, v1.Config{
