b9868a5bbb47d90c1c9d09fe01cba30c9b4db42d
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-18 23:33:48 +0100

    Add helper service to make account ID to name mapping simpler

---
+++ core/nameservice/nameservice.go
@@ -0,0 +1,64 @@
+package nameservice
+
+import (
+	"errors"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/tchorzewski1991/fitbit/core/blockchain/database"
+	"io/fs"
+	"path/filepath"
+	"strings"
+)
+
+// NameService provides lookup functionality for
+// account ID to account name mapping.
+type NameService struct {
+	accounts map[database.AccountID]string
+}
+
+// New constructs a new NameService.
+func New(accountsPath string) (*NameService, error) {
+	accounts := make(map[database.AccountID]string)
+
+	fn := func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return errors.New("accounts path is not valid")
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if filepath.Ext(path) != ".ecdsa" {
+			return errors.New("detected not supported file ext")
+		}
+
+		priv, err := crypto.LoadECDSA(path)
+		if err != nil {
+			return err
+		}
+
+		accountID, err := database.PubToAccountID(priv.PublicKey)
+		if err != nil {
+			return err
+		}
+
+		accounts[accountID] = strings.TrimSuffix(info.Name(), ".ecdsa")
+
+		return nil
+	}
+	err := filepath.Walk(accountsPath, fn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &NameService{accounts: accounts}, nil
+}
+
+// FindName returns the account name associated with given account ID.
+// It returns given account ID if associated account name does not exist.
+func (ns *NameService) FindName(accountID database.AccountID) string {
+	if accountName, exist := ns.accounts[accountID]; exist {
+		return accountName
+	}
+	return accountID.String()
+}
---
+++ core/nameservice/nameservice_test.go
@@ -0,0 +1,30 @@
+package nameservice_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/tchorzewski1991/fitbit/core/nameservice"
+	"testing"
+)
+
+func TestNameService(t *testing.T) {
+
+	// Ensure name service checks whether accounts path exists.
+	_, err := nameservice.New("testdata/accounts/broken")
+	assert.EqualError(t, err, "accounts path is not valid")
+
+	// Ensure name service checks whether account file has supported extension.
+	_, err = nameservice.New("testdata/accounts/invalid/test.invalid")
+	assert.EqualError(t, err, "detected not supported file ext")
+
+	// Ensure name service does not complain when accounts path exists.
+	ns, err := nameservice.New("testdata/accounts/valid/")
+	assert.Nil(t, err)
+
+	// Ensure name service fallbacks to the given account ID if account name not found.
+	name := ns.FindName("not-found")
+	assert.Equal(t, "not-found", name)
+
+	// Ensure name service returns account name for existing account ID.
+	name = ns.FindName("0x0ee5ba68586c85880B0900D0dEe0eEcBB37010e0")
+	assert.Equal(t, "test", name)
+}
