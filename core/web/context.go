bc4baadba72d7251be14e661fea058024f097f65
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-01-30 22:48:44 +0100

    Add missing comments under core web layer

--- core/web/context.go
+++ core/web/context.go
@@ -10,11 +10,14 @@ type ctxKey int
 
 const ctxValuesKey ctxKey = 1
 
+// Values encapsulates the details of the request which will be shared across program
+// boundaries within the context.
 type Values struct {
 	TraceID   string
 	StartedAt time.Time
 }
 
+// GetCtxValues retrieves instance of context Values out of given context.
 func GetCtxValues(ctx context.Context) (*Values, error) {
 	v, ok := ctx.Value(ctxValuesKey).(*Values)
 	if !ok {
@@ -23,6 +26,7 @@ func GetCtxValues(ctx context.Context) (*Values, error) {
 	return v, nil
 }
 
+// SetCtxValues extends given context with new instance of context Values.
 func SetCtxValues(ctx context.Context, values *Values) context.Context {
 	return context.WithValue(ctx, ctxValuesKey, values)
 }
--- core/web/models.go
+++ core/web/models.go
@@ -4,15 +4,18 @@ const (
 	success = "success"
 )
 
+// Response encapsulates the details of generic web response used withing the app.
 type Response struct {
 	Message string `json:"message,omitempty"`
 	Error   string `json:"error,omitempty"`
 }
 
+// Error is a constructor for generic web error response used by various http endpoints.
 func Error(err error) Response {
 	return Response{Error: err.Error()}
 }
 
+// Success is a constructor for generic web success response used by various http endpoints.
 func Success() Response {
 	return Response{Message: success}
 }
