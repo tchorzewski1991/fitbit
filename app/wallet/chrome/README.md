086929d8909cd7647bd8a5bf129d7b05c1a307e7
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2023-02-06 23:00:57 +0100

    Init new react app for chrome wallet extension

---
+++ app/wallet/chrome/README.md
@@ -0,0 +1,11 @@
+# Fitbit Wallet Chrome Extension 
+
+## Available Scripts
+
+### `npm start`
+
+### `npm test`
+
+### `npm run build`
+
+### `npm run eject`
---
+++ app/wallet/chrome/src/App.tsx
@@ -0,0 +1,14 @@
+import React  from 'react';
+import ShowGenesis from "./components/showGenesis"
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <ShowGenesis />
+    </div>
+  );
+}
+
+export default App;
